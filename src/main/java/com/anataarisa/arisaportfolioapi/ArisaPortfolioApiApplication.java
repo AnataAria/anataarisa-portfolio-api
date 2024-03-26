6dccb11e6f0d584dae2660d97507e4e3dd32f9df
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-03-26 21:21:13 +0700

    vinh/init springboot project api

---
+++ src/main/java/com/anataarisa/arisaportfolioapi/ArisaPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.anataarisa.arisaportfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArisaPortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArisaPortfolioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anataarisa/arisaportfolioapi/ArisaPortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anataarisa.arisaportfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArisaPortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
